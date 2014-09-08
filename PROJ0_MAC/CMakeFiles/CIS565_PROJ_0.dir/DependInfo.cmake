8110aff5636a118ae76a602262fc8ed1d3edaddc
Jiawei Wang <wangjiaweisdhr@gmail.com>
2014-09-08 15:35:39 -0400

    Project0 finished
    Screen shot is attached in the folder.

---
+++ PROJ0_MAC/CMakeFiles/CIS565_PROJ_0.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/jiaweiwang/Documents/CGGT/CIS565/Project-0/external/src/glslUtil/glslUtility.cpp" "/Users/jiaweiwang/Documents/CGGT/CIS565/Project-0/PROJ0_MAC/CMakeFiles/CIS565_PROJ_0.dir/external/src/glslUtil/glslUtility.cpp.o"
+  "/Users/jiaweiwang/Documents/CGGT/CIS565/Project-0/src/main.cpp" "/Users/jiaweiwang/Documents/CGGT/CIS565/Project-0/PROJ0_MAC/CMakeFiles/CIS565_PROJ_0.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../external/include"
+  "../external/src"
+  "/Developer/NVIDIA/CUDA-6.5/include"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ PROJ0_MAC/CMakeFiles/CIS565_PROJ_0.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CIS565_PROJ_0.dir/src/./CIS565_PROJ_0_generated_kernel.cu.o"
+  "CMakeFiles/CIS565_PROJ_0.dir/./CIS565_PROJ_0_intermediate_link.o"
+  "CMakeFiles/CIS565_PROJ_0.dir/src/main.cpp.o"
+  "CMakeFiles/CIS565_PROJ_0.dir/external/src/glslUtil/glslUtility.cpp.o"
+  "CIS565_PROJ_0.pdb"
+  "CIS565_PROJ_0"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CIS565_PROJ_0.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
